5e92f90a2f287b5f7ee5aeb3261d83dab4663b0d
Daonan <zdnzhangdaonan@gmail.com>
2024-01-06 21:18:57 +0100

    add one script and statics of the most common source web

--- Scripts/parallel/sciencedaily.py
+++ Scripts/parallel/sciencedaily.py
@@ -1,7 +1,8 @@
+import random
+
 import requests
 from bs4 import BeautifulSoup as bs
 from selenium.webdriver.support.wait import WebDriverWait
-
 from Scripts.utils import tools
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -9,6 +10,7 @@ import time
 import os
 import csv
 from selenium.webdriver.common.by import By
+from urllib.parse import urlparse
 
 root_path = '../../ParallelCorpus'
 name = 'ScienceDaily'
@@ -21,94 +23,125 @@ tools.make_dir(root_path, name)
 #           'plants_animals','earth_climate','fossils_ruins',
 #           'science_society','business_industry','education_learning']
 
+# for test
 topics = ['health_medicine']
 hrefs = []
 
-
 # create files to store hrefs
-# for topic in topics:
-#
-#     csv_file_path = os.path.join(root_path, name, name + f'{topic}.csv')
-#
-#     href_csv = folder_name + f'/hrefs_{topic}.csv'
-#     if not os.path.exists(href_csv):
-#         with open(href_csv, 'w') as csv_file:
-#             pass
-#     else:
-#         with open(folder_name + f'/hrefs_{topic}.csv', 'r') as f:
-#             hrefs = f.read().splitlines()
-#
-#
-#     href_csv = folder_name + f'/hrefs_{topic}.csv'
-#     if not os.path.exists(href_csv):
-#         with open(href_csv, 'w') as csv_file:
-#             pass
-#     else:
-#         with open(folder_name + f'/hrefs_{topic}.csv', 'r') as f:
-#             hrefs = f.read().splitlines()
+for topic in topics:
+
+    # source_domain = {}
+    csv_file_path = os.path.join(root_path, name, name + f'{topic}.csv')
+
+    href_csv = folder_name + f'/hrefs_{topic}.csv'
+    if not os.path.exists(href_csv):
+        with open(href_csv, 'w') as csv_file:
+            pass
+    else:
+        with open(folder_name + f'/hrefs_{topic}.csv', 'r') as f:
+            hrefs = f.read().splitlines()
+
+
+    href_csv = folder_name + f'/hrefs_{topic}.csv'
+    if not os.path.exists(href_csv):
+        with open(href_csv, 'w') as csv_file:
+            pass
+    else:
+        with open(folder_name + f'/hrefs_{topic}.csv', 'r') as f:
+            hrefs = f.read().splitlines()
 
     # for load more articles
-#     options = webdriver.ChromeOptions()
-#     options.binary_location = r"C:\Program Files\Google\Chrome\Application\chrome.exe"
-#     driver = webdriver.Chrome()
-#     driver.get(f"https://www.sciencedaily.com/news/{topic}/")
-#     time.sleep(3)
-#
-#     # accept cookies
-#     try:
-#         driver.find_element(By.CLASS_NAME, "sc-qRumB.bcoUVc.amc-focus-first").click()
-#     except:
-#         pass
-#
-#
-#     # load how many times more
-#     for i in range(1):
-#         driver.find_element(By.ID,'load_more_stories').click()
-#         time.sleep(8)
-#
-#
-#     soup = bs(driver.page_source, 'lxml')
-#     link_elements = driver.find_elements(By.CSS_SELECTOR, "div.latest-head a")
-#
-#
-#     for element in link_elements:
-#         href = element.get_attribute('href')
-#         if href not in hrefs:
-#             hrefs.append(href)
-#
-#     with open(folder_name + f'/hrefs_{topic}.csv', 'w') as f:
-#         for href in hrefs:
-#             f.write(href + '\n')
-#
-# #     success load hrefs
-#
-#: TODO: for each url in hrefs
+    options = webdriver.ChromeOptions()
+    options.binary_location = r"C:\Program Files\Google\Chrome\Application\chrome.exe"
+    driver = webdriver.Chrome()
+    driver.get(f"https://www.sciencedaily.com/news/{topic}/")
+    time.sleep(3)
 
-# example url
-url = 'https://www.sciencedaily.com/releases/2023/12/231207161449.htm'
-
-hdr = {'User-Agent': 'Mozilla/5.0'}
-r = requests.get(url, headers=hdr)
-source = r.content
-soup = bs(source, 'lxml')
-
-head = soup.find('h1', class_='headline').get_text()
-summaries = soup.find('dd', id='abstract').get_text()
-# empty by default
-reference = ''
-
-try:
-    reference = soup.find('ol', class_='journal').get_text()
-except:
-    pass
-
-print(reference)
-
-# if reference exist
-if(reference != ''):
-    pass
+    # accept cookies
+    try:
+        driver.find_element(By.CLASS_NAME, "sc-qRumB.bcoUVc.amc-focus-first").click()
+    except:
+        pass
 
 
-# :TODO: how relevant is the reference to the article, give a score from 0-100 via CHATGPT
+    # load how many times more
+    for i in range(1):
+        driver.find_element(By.ID,'load_more_stories').click()
+        time.sleep(4)
 
 
+    soup = bs(driver.page_source, 'lxml')
+    link_elements = driver.find_elements(By.CSS_SELECTOR, "div.latest-head a")
+
+    for element in link_elements:
+        href = element.get_attribute('href')
+        if href not in hrefs:
+            hrefs.append(href)
+
+    with open(folder_name + f'/hrefs_{topic}.csv', 'w') as f:
+        for href in hrefs:
+            f.write(href + '\n')
+
+    print('Load hrefs done')
+
+
+
+
+    # checking the domain of the source web page for each href in hrefs and write to csv
+    # picking 2-3 most popular domain to write scirpt ad hoc
+    for index, urls in enumerate(hrefs):
+        hdr = {'User-Agent': 'Mozilla/5.0'}
+        r = requests.get(urls, headers=hdr)
+        source = r.content
+        soup = bs(source, 'lxml')
+        pls_summaries = soup.find('dd', id='abstract').get_text()
+
+
+
+        # empty by default
+        reference = ''
+        # may don't have the reference
+        try:
+            a_tag = soup.find('ol', class_='journal').find_all('a')
+            if len(a_tag) == 1:
+                reference += a_tag[0].get('href')
+        except:
+            pass
+
+        # if reference exist
+        if (reference != ''):
+            # for estimate the source web domain
+            response = requests.get(reference ,headers = hdr)
+            if response.status_code == 200:
+                parsed_url = urlparse(response.url)
+                first_level = parsed_url.netloc
+                # only extract the abstract from nature
+                if 'nature' in first_level:
+                    source = response.content
+                    soup = bs(source, 'lxml')
+                    scientific_abstract = soup.find('div', class_='c-article-section__content').get_text()
+
+        # TODO: After makeing sure the reference exits,
+        #  write the summaries in one file with the unique id as PLS corpus
+        #  after extract the abstract from the reference web page:
+        #  write the abstract in other file with the same id as Scientific Corpus
+        #  Another thing is to check how relevant these two texts are, make set a thresh hold to filter out the irrelevant ones
+
+    #
+    #             if first_level in source_domain:
+    #                 count = source_domain[first_level]
+    #                 source_domain[first_level] = count + 1
+    #             else:
+    #                 source_domain[first_level] = 1
+    #         else:
+    #             pass
+    # print('Writing Source Domain')
+    # # after one topic loop
+    # domain_csv = os.path.join(folder_name, f'source_{topic}_web.csv')
+    # with open(domain_csv, 'w', newline='') as file:
+    #     writer = csv.writer(file)
+    #     writer.writerow(['Web', 'Count'])
+    #     for key, count in source_domain.items():
+    #         writer.writerow([key, count])
+
+    driver.close()
